b3345fb1f64d3950d06e62373d6d1111e412cdc4
Marquillos-99 <marckgit@gmail.com>
2023-07-26 14:18:14 -0600

    avances

---
+++ src/data/members.js
@@ -0,0 +1,22 @@
+/* eslint-disable indent */
+export const members = [{
+	title: 'Eva',
+    profesion: 'Ama de casa',
+    img: 'https://i.pinimg.com/736x/ac/e6/cf/ace6cfc54424429f3ed4783e99b708e3.jpg'
+},
+{
+	title: 'Javier',
+    profesion: 'Obrero',
+    img: 'https://static.wikia.nocookie.net/disney/images/1/1e/Coco_Hector_render.png'
+},
+{
+	title: 'Ema',
+    profesion: 'Granjera',
+    img: 'https://i.pinimg.com/736x/ac/e6/cf/ace6cfc54424429f3ed4783e99b708e3.jpg'
+},
+{
+	title: 'Gabriel',
+    profesion: 'Granjero',
+    img: 'https://static.wikia.nocookie.net/disney/images/1/1e/Coco_Hector_render.png'
+}
+]
